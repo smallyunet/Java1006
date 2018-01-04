8b2c1aa1e2534c29c9e73f92b341069161272a9f
smallyunet <499503799@qq.com>
2018-01-04 21:45:09 +0800

    build a hacker news front page

--- MERN/build-a-hn-front-page/app/NewsList.js
+++ MERN/build-a-hn-front-page/app/NewsList.js
@@ -2,14 +2,23 @@
 import React from 'react';
 import NewsHeader from './NewsHeader.js';
 import NewsItem from './NewsItem';
+import './NewsList.css';
 
 export default class NewsList extends React.Component {
-	render() {
+	  render() {
 		return (
-			<div className="newsList">
-				<NewsHeader />
-				<NewsItem />
-			</div>
-			);
-	}
+		    <div className="newsList">
+		      <NewsHeader />
+		      <div className="newsList-newsItem">
+		        {
+		          (this.props.items).map(function(item, index) {
+		            return (
+		                <NewsItem key={item.id} item={item} rank={index+1} />
+		                );
+		          })
+		        }
+		      </div>
+		    </div>
+		    );
+		}
 }
